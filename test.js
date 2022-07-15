fc59b6351f2252db8555dee7067ca10e57876ec6
littlekimber <zhyan0096@gmail.com>
2022-07-14 23:27:35 -0500

    2022-7-14

--- test.js
+++ test.js
@@ -1,26 +1,42 @@
-var decodeString = function(s) {
-    let stack = [];
-    for(let value of s){
-        console.log(stack);
-        if(value!=']'){
-            stack.push(value)
-        }else{
-            let temp = '';
-            let times = '';
-            while(stack[stack.length-1]!='['){
-                temp += stack.pop();
-            }
-            stack.pop();
-            while(stack.length>0 && !isNaN(stack[stack.length-1])){
-                times += stack.pop();
-            }
-            times = Number.parseInt(times.split('').reverse().join(''));
-            temp = temp.split("").reverse().join("").repeat(times);;
-            stack.push(temp);
+var canFinish = function(numCourses, prerequisites) {
+    const toGraph = (num, pre) => {
+          let adjacency = new Array(num);
+          for(let i=0; i<num; i++){
+              adjacency[i] = [];
+          }  
+          for(let i=0; i<pre.length; i++){
+              adjacency[pre[i][0]].push(pre[i][1])
+          }
+          return adjacency;
+    }  
+    const dfs = (graph, node, visited) => {
+        if(node)console.log(graph[node], visited)
+        if(visited.includes(node)) return false;
+        if(graph[node].length===0) return true;
+        visited.push(node);
+        // console.log(visited)
+        for(let i=0; i<graph[node].length; i++){
+            if(!dfs(graph, graph[node][i], visited)) return false;
         }
-    }   
+        // console.log(node)
+        let index = visited.indexOf(node);
+        visited.splice(index, 1)
+        // if(node===1)console.log(visited)
+        graph[node].length = [];
+        return true;
+    }
+    let graph = toGraph(numCourses, prerequisites);
+    let visited = [];
     
-    return stack.join('');
+    for(let i=0; i<numCourses; i++){
+        
+        if(!dfs(graph, i, visited)) return false;
+    }
+    dfs(graph, 1, visited)
+    console.log(visited)
+    return true;
 };
 
-decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef");
+console.log(canFinish(
+    5,
+    [[1,4],[2,4],[3,1],[3,2]]))
