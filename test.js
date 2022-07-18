d5ed660b81c4c54c71fab1535213b8679b537014
littlekimber <zhyan0096@gmail.com>
2022-07-17 23:59:38 -0500

    2022-7-17

--- test.js
+++ test.js
@@ -1,42 +1,24 @@
-var canFinish = function(numCourses, prerequisites) {
-    const toGraph = (num, pre) => {
-          let adjacency = new Array(num);
-          for(let i=0; i<num; i++){
-              adjacency[i] = [];
-          }  
-          for(let i=0; i<pre.length; i++){
-              adjacency[pre[i][0]].push(pre[i][1])
-          }
-          return adjacency;
-    }  
-    const dfs = (graph, node, visited) => {
-        if(node)console.log(graph[node], visited)
-        if(visited.includes(node)) return false;
-        if(graph[node].length===0) return true;
-        visited.push(node);
-        // console.log(visited)
-        for(let i=0; i<graph[node].length; i++){
-            if(!dfs(graph, graph[node][i], visited)) return false;
-        }
-        // console.log(node)
-        let index = visited.indexOf(node);
-        visited.splice(index, 1)
-        // if(node===1)console.log(visited)
-        graph[node].length = [];
-        return true;
-    }
-    let graph = toGraph(numCourses, prerequisites);
-    let visited = [];
-    
-    for(let i=0; i<numCourses; i++){
-        
-        if(!dfs(graph, i, visited)) return false;
-    }
-    dfs(graph, 1, visited)
-    console.log(visited)
-    return true;
+var findKthLargest = function(nums, k) {
+    k = nums.length - k;
+    return quickSelect(k, nums, 0, nums.length-1);
 };
 
-console.log(canFinish(
-    5,
-    [[1,4],[2,4],[3,1],[3,2]]))
+const quickSelect = (k, nums, left, right) => {
+    let pivot = nums[right], p = left;
+    for(let i=left; i<right; i++){
+        if(nums[i]<=pivot){
+            [nums[i], nums[p]] = [nums[p], nums[i]];
+            p += 1;
+        }
+    }
+    [nums[p], nums[right]] = [nums[right], nums[p]];
+    console.log(nums)
+    if(p<k){
+        return quickSelect(k, nums, p+1, right);
+    }else if(p>k){
+        return quickSelect(k, nums, left, p-1);
+    }else{
+        return nums[p];
+    }
+}
+console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 4))
