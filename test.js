029472f6c19a5dea43a91a85eb3e8e16dda45125
littlekimber <zhyan0096@gmail.com>
2022-07-19 08:24:18 -0500

    2022-07-18

--- test.js
+++ test.js
@@ -1,24 +1,5 @@
-var findKthLargest = function(nums, k) {
-    k = nums.length - k;
-    return quickSelect(k, nums, 0, nums.length-1);
-};
-
-const quickSelect = (k, nums, left, right) => {
-    let pivot = nums[right], p = left;
-    for(let i=left; i<right; i++){
-        if(nums[i]<=pivot){
-            [nums[i], nums[p]] = [nums[p], nums[i]];
-            p += 1;
-        }
-    }
-    [nums[p], nums[right]] = [nums[right], nums[p]];
-    console.log(nums)
-    if(p<k){
-        return quickSelect(k, nums, p+1, right);
-    }else if(p>k){
-        return quickSelect(k, nums, left, p-1);
-    }else{
-        return nums[p];
-    }
+const Mystack = function(){
+    this.length = 0;
 }
-console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 4))
+console.log(Mystack.length)
+
