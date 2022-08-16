6a4f9b4b7693b7447d3668498ec2eabb3eda0f56
littlekimber <zhyan0096@gmail.com>
2022-08-15 23:43:49 -0500

    2022 8 15

--- test.js
+++ test.js
@@ -1,5 +1,37 @@
-const Mystack = function(){
-    this.length = 0;
-}
-console.log(Mystack.length)
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+ var evalRPN = function(tokens) {
+    let stack = [];
+    for(let token of tokens){
+        if(!isNaN(token)){
+            stack.push(token);
+        }else{
+            if(token==="+"){
+                let firstOpprand = stack.pop();
+                let secondOpprand = stack.pop();
+                let temp = Number.parseInt(secondOpprand, 10) + Number.parseInt(firstOpprand, 10);
+                stack.push(temp);
+            }else if(token==="-"){
+                let firstOpprand = stack.pop();
+                let secondOpprand = stack.pop();
+                let temp = Number.parseInt(secondOpprand, 10) - Number.parseInt(firstOpprand, 10);
+                stack.push(temp);
+            }else if(token==="/"){
+                let firstOpprand = stack.pop();
+                let secondOpprand = stack.pop();
+                let temp = Number.parseInt(secondOpprand, 10) / Number.parseInt(firstOpprand, 10);
+                stack.push(Number.parseInt(temp, 10));
+            }else{
+               let firstOpprand = stack.pop();
+                let secondOpprand = stack.pop();
+                let temp = Number.parseInt(secondOpprand, 10) * Number.parseInt(firstOpprand, 10);
+                stack.push(temp);
+            }
+        }
+    }
+    return Number.parseInt(stack.pop(), 10);
+};
 
+console.log(evalRPN(["4","13","5","/","+"]))
